d8f411e47243782a8554181f7d15546ed3c0b9bb
teresa229 <scarlett_29@naver.com>
2020-12-24 02:10:03 +0900

    수정

--- Practice/Practice05.sql
+++ Practice/Practice05.sql
@@ -32,8 +32,6 @@ and manager_id is not null;
 --직원번호(employee_id), 이름(first_name), 급여(salary), 입사일(hire_date), 전화번호(phone_number), 부서번호(department_id) 를 조회
 --조건절비교
 --급여의 내림차순
-select *
-from employees;
 
 select employee_id "직원번호",
        first_name "이름",
@@ -62,44 +60,86 @@ order by salary desc;
 --매니저아이디, 매니저이름(first_name), 매니저별평균급여, 매니저별최소급여, 매니저별최대급여
 
 select manager_id,
-      -- first_name,
+      --first_name,
        round(avg(salary),1),
        min(salary),
        max(salary)
 from employees
 where hire_date > '2005/01/01'
-group by manager_id;     16명으로 추려짐.
+and salary >= 5000
+group by manager_id
+order by avg(salary) desc;     16명으로 추려짐.
 
 --테이블에 조인 x
-select emp.manager_id,
-       emp.first_name,
-       mana.round(avg(salary),1),
-       mana.min(salary),
-       mana.max(salary)
-from employees emp, (select manager_id,
-                            round(avg(salary),1),
-                            min(salary),
-                            max(salary)
-                     from employees
-                     where hire_date > '2005/12/31'
-                     group by manager_id) mana
-where emp.manager_id = mana.manager_id;
+select e.manager_id,
+       e.first_name,
+       e.salary,
+       m.salary,
+       min.salary,
+       max.salary
+from employees e, (select manager_id,
+                          avg(salary) as salary
+                   from employees
+                   where hire_date > '2005/12/31'
+                   and salary >= 5000
+                   group by manager_id
+                   order by salary desc) m,
+                   (select manager_id,
+                           min(salary) as salary
+                   from employees
+                   where hire_date > '2005/12/31'
+                   and salary >= 5000
+                   group by manager_id
+                   order by salary desc) min,
+                   (select manager_id,
+                           max(salary) as salary
+                   from employees
+                   where hire_date > '2005/12/31'
+                   and salary >= 5000
+                   group by manager_id
+                   order by salary desc) max
+where e.manager_id = m.manager_id;
+
+
+select *
+from employees;
+
+-- 맞나?
+select m.manager_id,
+       e.first_name,
+       m.salary,
+       e.mins,
+       e.maxs
+from employees e,(select manager_id,
+                         avg(salary) salary,
+                         min(salary) mins,
+                         max(salary) maxs
+                  from employees
+                  where hire_date > '2005/01/01'
+                  and salary >= 5000
+                  group by manager_id
+                  order by avg(salary) desc;) m
+where e.employee_id = m.manager_id;
+
+
+
 
 --조건절에 조인
-select manager_id,
-       first_name,
-       round(avg(salary),1),
-       min(salary),
-       max(salary)
-from employees emp
-where avg(salary) > 5000
-and (manager_id,salary )in (select manager_id,
-                                   round(avg(salary),1),
-                                   min(salary),
-                                   max(salary)
-                              from employees
-                              where hire_date > '2005/12/31'
-                              group by manager_id);
+select e.manager_id,
+       e.first_name,
+       e.salary
+    --   e.min(salary),
+    --   e.max(salary)
+from employees e, departments d
+where e.employee_id = d.manager_id
+and e.salary = m.salary
+and   (manager_id, salary )in (select manager_id,
+                                      avg(salary) as salary
+                               from employees
+                               where hire_date > '2005/12/31'
+                               and salary >= 5000
+                               group by manager_id
+                               order by avg(salary) desc);
                               
 /*                            
 문제4.
@@ -125,11 +165,11 @@ from employees; -- 178	Kimberely	149
 select em.employee_id,
        em.first_name,
        de.department_name,
-       em.manager_id,
-       em.department_id
+       man.manager_id,
+       man.first_name       
 from employees em, employees man, departments de
 where em.department_id = de.department_id
-and em.employee_id= man.manager_id ;
+and em.employee_id= man.manager_id;
 
 
 /*
@@ -238,7 +278,15 @@ from  (select rownum rnum,
 where rnum >= 1
 and rnum < 3 ;
 
-
+-- 답
+select emp.first_name || ' ' || emp.last_name "직원이름",
+       emp.salary "연봉",
+       dep.department_name "부서이름",
+       emp.hire_date
+from employees emp, departments dep
+where emp.department_id = dep.department_id
+and emp.hire_date = (select max(hire_date)
+                     from employees); 
 
 /*
 문제7.
@@ -250,29 +298,97 @@ and rnum < 3 ;
 select *
 from employees;
 
---부서별 평균연봉 정보
-select avg(salary),
-       department_id
+--1)부서별 평균연봉 정보
+select department_id,
+       avg(salary)
 from employees
 group by department_id
 order by avg(salary) desc; -->department id = 90
 
+--2)부서별 가장 높은 연봉 조회
+select max(avg(salary))
+from employees
+group by department_id;
 
+--3)직원번호(employee_id), 이름(firt_name), 성(last_name)과  업무(job_title), 연봉(salary)을 조회
+select employee_id "직원번호",
+       first_name "이름",
+       last_name "성",
+       salary "연봉",
+       job_title "업무"
+from employees em, jobs jo
+where em.job_id= jo.job_id;
+
+--합치기 1)+2)
+select a.department_id,
+       a.salary
+from(select department_id,
+       avg(salary) as salary
+     from employees
+     group by department_id) a,(select max(avg(salary)) as salary
+                                from employees
+                                group by department_id) b
+where a.salary = b.salary;
+
+--답
+select em.employee_id "직원번호",
+       em.first_name "이름",
+       em.last_name "성",
+       em.salary "연봉",
+       jo.job_title "업무"
+from employees em, jobs jo, (select a.department_id,
+                                    a.salary
+                             from(select department_id,
+                                         avg(salary) as salary
+                                  from employees
+                                  group by department_id) a,(select max(avg(salary)) as salary
+                                                             from employees
+                                                             group by department_id) b
+                             where a.salary = b.salary) area
+where em.job_id = jo.job_id 
+and em.department_id = area.department_id;
 
 /*
 문제8.
 평균 급여(salary)가 가장 높은 부서는? 
 */ 
---부서별 평균연봉 정보
-select de.department_id,
-       de.department_name,
-       em.employee_id,
-       avg(salary)
-from employees em, departments de
-where em.department_id = de.department_id
-order by avg(salary) desc;
+--부서명
 
---order by avg(salary) desc;
+--1)부서별 평균급여 정보
+select department_id,
+       avg(salary)
+from employees
+group by department_id;
+
+--2)평균급여가 가장 높은 부서
+select max(avg(salary))
+from employees
+group by department_id;
+
+--3)합치기 1+2
+select a.department_id,
+       a.salary
+from (select department_id,
+             avg(salary) as salary
+      from employees
+      group by department_id) a, (select max(avg(salary)) as salary
+                                  from employees
+                                  group by department_id) b
+where a.salary = b.salary;
+
+--departments 연결
+select d.department_id,
+       d.department_name
+from departments d, (select a.department_id,
+                            a.salary
+                     from (select department_id,
+                                  avg(salary) as salary
+                           from employees
+                           group by department_id) a, (select max(avg(salary)) as salary
+                                                       from employees
+                                                       group by department_id) b
+                     where a.salary = b.salary) area
+where area.department_id = d.department_id;
 
 
 /*
@@ -280,6 +396,37 @@ order by avg(salary) desc;
 평균 급여(salary)가 가장 높은 지역은? 
 */
 
+--location_id 확인
+select d.department_id,
+       d.department_name,
+       d.location_id
+from departments d, (select a.department_id,
+                            a.salary
+                     from (select department_id,
+                                  avg(salary) as salary
+                           from employees
+                           group by department_id) a, (select max(avg(salary)) as salary
+                                                       from employees
+                                                       group by department_id) b
+                     where a.salary = b.salary) area
+where area.department_id = d.department_id;
+
+--departments, locations 연결
+select location_id
+from locations lo,(select d.department_id,
+                          d.department_name,
+                          d.location_id as location
+                   from departments d, (select a.department_id,
+                                               a.salary
+                                        from (select department_id,
+                                                     avg(salary) as salary
+                                              from employees
+                                              group by department_id) a, (select max(avg(salary)) as salary
+                                                                          from employees
+                                                                          group by department_id) b
+                                        where a.salary = b.salary) area
+                   where area.department_id = d.department_id) darea
+where lo.location_id = darea.location;
 
 
 /*
